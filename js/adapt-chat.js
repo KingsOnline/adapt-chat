77fe9aca9dd5e1fac7096b585aace01fa14f8f3e
Simon Date <simondate@yahoo.co.uk>
2017-09-27 15:49:04 +0100

    Added icon with the participatns. Furhter styling and refactoring applied.

--- js/adapt-chat.js
+++ js/adapt-chat.js
@@ -1,94 +1,102 @@
 define(function(require) {
 
-	var ComponentView = require('coreViews/componentView');
-	var Adapt = require('coreJS/adapt');
+  var ComponentView = require('coreViews/componentView');
+  var Adapt = require('coreJS/adapt');
 
-	var Chat = ComponentView.extend({
+  var Chat = ComponentView.extend({
 
-		events: {
-			"click .stacklist-next": "nextItem"
-		},
+    events: {
+      "click .stacklist-next": "nextItem"
+    },
 
-		preRender: function() {
-			this.model.set("_stage", -1);
-			this.setupButton();
-		},
+    preRender: function() {
+      this.model.set("_stage", -1);
+      this.setupButton();
+    },
 
-		postRender: function() {
-			if (!this.model.get("_isComplete") || this.model.get("_isResetOnRevisit")) this.setupListItems();
-			this.setReadyStatus();
-		},
+    postRender: function() {
+      if (!this.model.get("_isComplete") || this.model.get("_isResetOnRevisit")) this.setupListItems();
+      this.setReadyStatus();
+    },
 
-		setupButton: function() {
-			var _button = this.model.get("_button") || {};
+    setupButton: function() {
+      var _button = this.model.get("_button") || {};
 
-			if (!_button.startText) _button.startText = "Click here to begin";
-			if (!_button.continueText) _button.continueText = "Next";
+      if (!_button.startText) _button.startText = "Click here to begin";
+      if (!_button.continueText) _button.continueText = "Next";
 
-			this.model.set("_button", _button);
-		},
+      this.model.set("_button", _button);
+    },
 
-		setupListItems: function() {
-			var $stacklistItems = this.$(".stacklist-items");
-			$stacklistItems.height($stacklistItems.height());
-			var $items = this.$(".stacklist-item");
-			var wWin = $(window).width();
-			$items.each(function(i) {
-				var $el = $items.eq(i);
-				var even = i % 2 === 0;
-				var offset = $el.offset();
-				offset.left = even ? - ($el.outerWidth() + 10) : wWin + 10;
-				$el.offset(offset).hide();
-			});
-			this.$(".stacklist-button").show();
+    setupListItems: function() {
+      var $stacklistItems = this.$(".stacklist-items");
+      $stacklistItems.height($stacklistItems.height());
+      var $items = this.$(".stacklist-item");
+      var wWin = $(window).width();
+      $items.each(function(i) {
+        var $el = $items.eq(i);
+        var even = i % 2 === 0;
+        var offset = $el.offset();
+        offset.left = even ? -($el.outerWidth() + 10) : wWin + 10;
+        $el.offset(offset).hide();
+      });
+      this.$(".stacklist-button").show();
 
-			var context = this;
-			_.each(this.model.get('_items'), function(item){
-				context.setImage(item);
-			});
-		},
+      var context = this;
+      _.each(this.model.get('_items'), function(item, index) {
+        context.setImage(index, item);
+      });
+    },
 
-		setImage: function(item) {
-			console.log(item);
-		},
+    setImage: function(index, item) {
+      var $item = this.$('.chat-icon-inner').get(index);
+      console.log(this.model.get('_participants')[item._participant]);
+      $($item).attr('src', this.model.get('_participants')[item._participant]._icon);
+    },
 
-		nextItem: function() {
-			var stage = this.model.get("_stage") + 1;
-			this.setStage(stage);
-		},
+    nextItem: function() {
+      var stage = this.model.get("_stage") + 1;
+      this.setStage(stage);
+    },
 
-		setStage: function(stage) {
-			this.model.set("_stage", stage);
-			var continueText = this.model.get("_items")[stage].next || this.model.get("_button").continueText;
-			this.$(".stacklist-next").html(continueText);
-			var $item = this.$(".stacklist-item").eq(stage);
-			$item.show();
-			var h = $item.outerHeight(true);
+    setStage: function(stage) {
+      this.model.set("_stage", stage);
+      var continueText = this.model.get("_items")[stage].next || this.model.get("_button").continueText;
+      this.$(".stacklist-next").html(continueText);
+      var $item = this.$(".stacklist-item").eq(stage);
+      $item.show();
+      var h = $item.outerHeight(true);
 
-			this.$(".stacklist-button").css({top: "+=" + h});
-			setTimeout(function() {
-				$item.css({left: 0});
-			}, 250);
+      this.$(".stacklist-button").css({
+        top: "+=" + h
+      });
+      setTimeout(function() {
+        $item.css({
+          left: 0
+        });
+      }, 250);
 
-			if (this.model.get("_items").length - 1 === stage) {
-				this.onComplete()
-			}
-		},
+      if (this.model.get("_items").length - 1 === stage) {
+        this.onComplete()
+      }
+    },
 
-		onComplete: function () {
+    onComplete: function() {
 
-			var $button = this.$(".stacklist-button");
-			$button.css({top: $(window).height()});
-			setTimeout(function() {
-				$button.remove();
-			}, 500);
+      var $button = this.$(".stacklist-button");
+      $button.css({
+        top: $(window).height()
+      });
+      setTimeout(function() {
+        $button.remove();
+      }, 500);
 
-			this.setCompletionStatus();
-		}
-	});
+      this.setCompletionStatus();
+    }
+  });
 
-	Adapt.register('chat', Chat);
+  Adapt.register('chat', Chat);
 
-	return Chat;
+  return Chat;
 
 });
