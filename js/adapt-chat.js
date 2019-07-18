9eed68bc67bd78d11e828dcc9d8c9441329101a0
daniellejohnstoneKO <37866346+daniellejohnstoneKO@users.noreply.github.com>
2019-07-18 11:09:34 +0100

    Update adapt-chat.js

--- js/adapt-chat.js
+++ js/adapt-chat.js
@@ -68,7 +68,7 @@ define(function(require) {
 			this.model.set("_stage", stage);
 			var continueText = this.model.get("_items")[stage].next || this.model.get("_button").continueText;
 			this.$(".stacklist-next").html(continueText);
-			var $item = this.$(".stacklist-item").eq(stage);
+			var $item = this.$(".chat-line").eq(stage);
 			$item.show();
 			var h = $item.outerHeight(true);
 
